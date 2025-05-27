2cc94e1710aaae36c60d8747b68c1c0e69250c5b
Emily Zheng <61632678+emiz39@users.noreply.github.com>
2025-05-27 00:51:38 -0700

    updated MajorDAO

--- src/dao/MajorDAO.java
+++ src/dao/MajorDAO.java
@@ -1,6 +1,9 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MajorDAO {
     private final Connection conn;
@@ -9,15 +12,42 @@ public class MajorDAO {
         this.conn = conn;
     }
 
-    // public List<String> getAllMajors() throws SQLException {
-    //     // TODO: implement
-    // }
+    public List<String> getAllMajors() throws SQLException {
+        String sql = "SELECT major_id, name FROM MAJORS";
+        List<String> majors = new ArrayList<>();
+        try (var stmt = conn.prepareStatement(sql)) {
+            var rs = stmt.executeQuery();
+            while (rs.next()) {
+                String major = rs.getInt("major_id") + " - " + rs.getString("name");
+                majors.add(major);
+            }
+        }
+        return majors;
+    }
 
-    // public List<String> getMustTakeCourses(int majorId) throws SQLException {
-    //     // TODO: implement
-    // }
+    public List<String> getMustTakeCourses(int majorId) throws SQLException {
+        String sql = "SELECT course_no FROM MUST_TAKE WHERE major_id = ?";
+        List<String> mustTakeCourses = new ArrayList<>();
+        try (var stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, majorId);
+            var rs = stmt.executeQuery();
+            while (rs.next()) {
+                mustTakeCourses.add(rs.getString("course_no"));
+            }
+        }
+        return mustTakeCourses;
+    }
 
-    // public List<String> getElectiveOptions(int majorId) throws SQLException {
-    //     // TODO: implement
-    // }
+    public List<String> getElectiveOptions(int majorId) throws SQLException {
+        String sql = "SELECT course_no FROM CAN_TAKE WHERE major_id = ?";
+        List<String> electiveCourses = new ArrayList<>();
+        try (var stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, majorId);
+            var rs = stmt.executeQuery();
+            while (rs.next()) {
+                electiveCourses.add(rs.getString("course_no"));
+            }
+        }
+        return electiveCourses;
+    }
 }
